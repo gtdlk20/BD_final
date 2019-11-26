09575556d7ddb61cd0d121ad3415ddff5f8d2bdf
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-11-26 17:30:20 +0100

    Test

---
+++ SpotifyTest.py
@@ -0,0 +1,11 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
+
+client_id = "cd3c3b7e630f47d7b588b114a877e95e"
+client_secret = "40f77d47dab14786a80bd860b35ad4a8"
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
+
+name = "Kidz Bop Kids" #chosen artist
+result = sp.search(name) #search query
+print(result['tracks']['items'][0]['name'])
