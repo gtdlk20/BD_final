3803554b85053962b9569465dc78cfc899a2b5de
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-12-01 10:23:59 +0100

    Added some comments to improve the speed of the code

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -16,6 +16,9 @@ dfOriginal = pd.read_csv("OriginalTable.csv")
 #create new Afinn object for sentiment analysis
 af = Afinn()
 
+# YOU CAN USE INDEXING FUNCTIONS TO MAKE THIS RUN FASTER INSTEAD OF CREATING YOUR OWN METHOD WITH A FOR LOOP !!!!!!!!!!!!!!:
+# https://www.geeksforgeeks.org/python-count-of-elements-matching-particular-condition/ 
+# I like method 2 personally. your condition can be if the value is in swears[] !!!!!!!!!!!!!!!!!!!
 #returns number of basic swear words found in song lyrics
 def count_swears(lyrics):
 #param lyrics: a string of song lyrics
@@ -36,6 +39,9 @@ def get_basic_filthiness(lyrics):
     num_swears = count_swears(lyrics)
     return num_swears / (len(lyrics.split()) - num_swears)
 
+# IF YOU ARE RETURNING A LIST THE SAME SIZE AS THE INPUT USE MAP!!!!!!!!!!!!!!!!!!!!!!!!!!
+# https://www.geeksforgeeks.org/python-map-function/
+# IT IS MUCH MUCH FASTER THAN A SEPARATE METHOD WITH A FOR LOOP!!!!!!!!!
 #returns list of tuples (title, filth) where filth is the filth score of a song
 def get_df_filth(df):
     #param df: a pandas dataframe
@@ -45,7 +51,6 @@ def get_df_filth(df):
         lyrics = getattr(row, 'Lyrics')
         filth.append((title, get_basic_filthiness(lyrics)))
     return filth
-
 
 #preprocesses lyrics for sentiment analysis: removes all stopwords (from nltk) and numbers 
 #returns list of analyzable words
@@ -59,6 +64,8 @@ def filter_lyrics(lyrics):
             return False
 
     #removes punctuation from words
+    # USE REGULAR EXPRESSIONS TO REMOVE THESE!!!!!!!!!!!!!!!!!!!!
+    # re.sub([a-zA-Z0-9\s], '', word) for example will remove everything that not a letter, digit, or space
     def remove_punct(word):
         punct = ['.', ',', ':', ';', '(', ')', '_', '-', '?', '!', '/', '`']
         w = ''
@@ -68,11 +75,12 @@ def filter_lyrics(lyrics):
                 w += c
         return w
 
-    
     # Set words to lowercase and remove them if they are stop words, otherwise remove punctuation
     words = [remove_punct(w.lower()) for w in lyrics.split() if w.lower() not in stopwords]
 
     # Remove numbers
+    # You can just do a regular expression for this!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # re.sub([a-zA-Z\s], '', word) for example will remove anything thats not a letter or space
     words = [w for w in words if not _isnum(w)]
 
     return words
@@ -99,6 +107,7 @@ def get_lyrics_sentiment_score(lyrics):
     #return the average sentiment score over all evaluated words
     return score / len(words)
 
+# AGAIN LOOK INTO USING MAP FOR THIS
 #return list of tuples (title, sent_score) for dataframe
 def get_df_sent_scores(df):
     sent = []
@@ -107,7 +116,6 @@ def get_df_sent_scores(df):
         lyrics = getattr(row, 'Lyrics')
         sent.append((title, get_lyrics_sentiment_score(lyrics)))
     return sent
-
 
 #get filth scores of kidz bop- presumably zero
 kbFilth = get_df_filth(dfKidzBop)
@@ -123,7 +131,6 @@ def get_song_sent_graph(title, lyrics):
     plt.title('Sentiment score by word of %s' % title)
     plt.show()
 
-
 #returns histogram of sentiment scores
 def get_sent_hist(scores, header):
     #param scores: list of tuples containing song names and sentiment scores
@@ -131,7 +138,6 @@ def get_sent_hist(scores, header):
     plt.hist([score[1] for score in scores])
     plt.title(header)
     plt.show()
-
 
 #get sentiment scores for each kidzbop and original song
 kidzbop_sent_scores = get_df_sent_scores(dfKidzBop)
