a8c4cb41860802e756069fa39b9a2934588c70c4
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-12-02 09:47:19 +0100

    Added comparison file

---
+++ Comparison.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pylab as plt
+import re
+import numpy as np
+
+#reading in KidzBopTable as pandas dataframe
+dfKidzBop = pd.read_csv("KidzBopTable.csv") 
+
+#reading in OriginalTable as pandas dataframe
+dfOriginal = pd.read_csv("OriginalTable.csv")
