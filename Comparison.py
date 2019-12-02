0fcc1670be849bf78ae5895ad9e5ddffb866ffe5
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-12-02 11:53:57 +0100

    added

--- Comparison.py
+++ Comparison.py
@@ -9,5 +9,10 @@ dfKidzBop = pd.read_csv("KidzBopTable.csv")
 #reading in OriginalTable as pandas dataframe
 dfOriginal = pd.read_csv("OriginalTable.csv")
 
+#creating a new pandas dataframe
+df = pd.DataFrame(columns=['SentimentDifference'])
+
 sentimentDiff = np.array(list(dfOriginal["SentimentAverage"])) - np.array(list(dfKidzBop["SentimentAverage"]))
 print(sentimentDiff)
+
+dfDiff["SentimentDifference"] = sentimentDiff
