8fde7a1bad1b374fdaf13b5baf415b55eb8027b3
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-12-02 22:08:17 +0100

    Quick fixes

--- Comparison.py
+++ Comparison.py
@@ -13,19 +13,6 @@ dfOriginal = pd.read_csv("OriginalTable.csv")
 #creating a new pandas dataframe
 dfDiff = pd.DataFrame()
 
-#create list of available metrics to access
-stats = ['Popularity' , 'Acousticness', 'Danceability', 'Energy', 'Instrumentalness', 'Liveness', 'Loudness', 'Speechiness', 'Valence', 'Tempo', 'SwearCount', 'SentimentAverage']
-
-sentimentDiff = np.array(list(dfOriginal["SentimentAverage"])) - np.array(list(dfKidzBop["SentimentAverage"]))
-# print(sentimentDiff)
-
-dfDiff["SentimentDifference"] = sentimentDiff
-
-swearlessSentimentDiff = np.array(list(dfOriginal["SwearlessSentimentAverage"])) - np.array(list(dfKidzBop["SentimentAverage"]))
-# print(swearlessSentimentDiff)
-
-dfDiff["SwearlessSentimentDifference"] = swearlessSentimentDiff
-
 #determines which words are different from one set of lyrics to the next
 def compareLyrics(lyrics1, lyrics2):
     wordsList1 = np.array(lyrics1.lower().split())
@@ -34,29 +21,23 @@ def compareLyrics(lyrics1, lyrics2):
     lyricsDiff = np.setdiff1d(wordsList1,wordsList2)
     return lyricsDiff
 
-#make a list containing differences of stats for songs
-def makeDif(stat):
-#stat: the metric by which you would like to compare (str)
-#from the set {Popularity ,Acousticness ,Danceability ,Energy ,Instrumentalness ,Liveness ,Loudness ,Speechiness, /
-#Valence, Tempo, SwearCount, SentimentAverage}
-    difList = list(map(getDif, dfOriginal[stat], dfKidzBop[stat]))
-    return difList
-
-
-def getDif(stat1, stat2):
-    return math.fabs(stat1-stat2)
-
-
-#kidzBopLyricsDiff = list(map(compareLyrics, dfKidzBop["Lyrics"].split(), dfOriginal["Lyrics"].split()))
 originalLyricsDiff = list(map(compareLyrics, list(dfOriginal["Lyrics"]), list(dfKidzBop["Lyrics"])))
+# kidzBopLyricsDiff = list(map(compareLyrics, list(dfKidzBop["Lyrics"]), list(dfOriginal["Lyrics"])))
+
+dfDiff['DiffWords'] = originalLyricsDiff
+
+#create list of available metrics to access
+stats = ['Popularity' , 'Acousticness', 'Danceability', 'Energy', 'Instrumentalness', 'Liveness', 'Loudness', 'Speechiness', 'Valence', 'Tempo', 'SwearCount', 'SentimentAverage']
+
+#make a list containing differences of stats for songs
+#from the set {Popularity ,Acousticness ,Danceability ,Energy ,Instrumentalness ,Liveness ,Loudness ,Speechiness,...
+#Valence, Tempo, SwearCount, SentimentAverage}
+def makeDiff(column1, column2):
+    diffList = abs(np.array(column1) - np.array(column2))
+    return diffList
 
 for stat in stats:
-    dfDiff['diff%s' % stat] = makeDif(stat)
-
-dfDiff['diffWords'] = originalLyricsDiff
-
-#print(kidzBopLyricsDiff[0])
-#print(originalLyricsDiff[0])
+    dfDiff['Diff%s' % stat] = makeDiff(list(dfOriginal[stat]), list(dfKidzBop[stat]))
 
 # saving dataframe of differences to .csv
 dfDiff.to_csv('DifferenceTable.csv', index=False)
