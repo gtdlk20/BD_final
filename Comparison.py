0ab482fd53f1c556ae7f0733bc193d8b12324c07
Garrett Kaufmann <31054430+gtdlk20@users.noreply.github.com>
2019-12-03 18:29:39 +0100

    Update Comparison.py

--- Comparison.py
+++ Comparison.py
@@ -3,6 +3,7 @@ import matplotlib.pylab as plt
 import re
 import numpy as np
 import math
+from afinn import Afinn
 
 #reading in KidzBopTable as pandas dataframe
 dfKidzBop = pd.read_csv("KidzBopTable.csv") 
@@ -17,6 +18,7 @@ dfDiff['Title'] = list(dfOriginal['Title'])
 # determines which words are different from one set of lyrics to the next
 # WE NEED TO IMPROVE THIS METHOD
 def compareLyrics(lyrics1, lyrics2):
+    af = Afinn()
     #removes section headers from lyrics
     lyrics1 = re.sub("[\(\[].*?[\)\]]", "", lyrics1)
     lyrics2 = re.sub("[\(\[].*?[\)\]]", "", lyrics2)
@@ -24,6 +26,7 @@ def compareLyrics(lyrics1, lyrics2):
     wordsList2 = np.array(lyrics2.lower().split())
     # what words are in lyrics 1, but not lyrics 2
     lyricsDiff = np.setdiff1d(wordsList1,wordsList2)
+    lyricsDiff = [word for word in lyricsDiff if af.score(word) < 0]
     return lyricsDiff
 
 originalLyricsDiff = list(map(compareLyrics, list(dfOriginal["Lyrics"]), list(dfKidzBop["Lyrics"])))
