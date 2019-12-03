a375af14ba7ea28683abc51bae3a20255b10cc7b
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-12-03 18:57:30 +0100

    Word differences broken down

--- Comparison.py
+++ Comparison.py
@@ -4,6 +4,8 @@ import re
 import numpy as np
 import math
 from afinn import Afinn
+    
+af = Afinn()
 
 #reading in KidzBopTable as pandas dataframe
 dfKidzBop = pd.read_csv("KidzBopTable.csv") 
@@ -18,21 +20,24 @@ dfDiff['Title'] = list(dfOriginal['Title'])
 # determines which words are different from one set of lyrics to the next
 # WE NEED TO IMPROVE THIS METHOD
 def compareLyrics(lyrics1, lyrics2):
-    af = Afinn()
-    #removes section headers from lyrics
-    lyrics1 = re.sub("[\(\[].*?[\)\]]", "", lyrics1)
-    lyrics2 = re.sub("[\(\[].*?[\)\]]", "", lyrics2)
+    #removes section headers and punctuation from lyrics
+    lyrics1 = re.sub("[\(\[].*?[\)\]]|[^a-zA-Z0-9\s]", "", lyrics1)
+    lyrics2 = re.sub("[\(\[].*?[\)\]]|[^a-zA-Z0-9\s]", "", lyrics2)
     wordsList1 = np.array(lyrics1.lower().split())
     wordsList2 = np.array(lyrics2.lower().split())
     # what words are in lyrics 1, but not lyrics 2
     lyricsDiff = np.setdiff1d(wordsList1,wordsList2)
-    lyricsDiff = [word for word in lyricsDiff if af.score(word) < 0]
     return lyricsDiff
 
 originalLyricsDiff = list(map(compareLyrics, list(dfOriginal["Lyrics"]), list(dfKidzBop["Lyrics"])))
-# kidzBopLyricsDiff = list(map(compareLyrics, list(dfKidzBop["Lyrics"]), list(dfOriginal["Lyrics"])))
+kidzBopLyricsDiff = list(map(compareLyrics, list(dfKidzBop["Lyrics"]), list(dfOriginal["Lyrics"])))
 
-dfDiff['DiffWords'] = originalLyricsDiff
+# filters word differences using sentiment score
+removedWords = list(map(lambda wordDiff: [word for word in wordDiff if af.score(word) < 0], originalLyricsDiff))
+addedWords = list(map(lambda wordDiff: [word for word in wordDiff if af.score(word) > 0], kidzBopLyricsDiff))
+
+dfDiff['RemovedWords'] = removedWords
+dfDiff['AddedWords'] = addedWords
 
 #create list of available metrics to access
 stats = ['Popularity' , 'Acousticness', 'Danceability', 'Energy', 'Instrumentalness', 'Liveness', 'Loudness', 'Speechiness', 'Valence', 'Tempo', 'SwearCount', 'SentimentAverage']
