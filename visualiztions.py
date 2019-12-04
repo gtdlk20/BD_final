938ed106dfbcd881630f3af5f12ff0a405534425
Garrett Kaufmann <31054430+gtdlk20@users.noreply.github.com>
2019-12-04 19:31:55 +0100

    added visualisations

--- visualiztions.py
+++ visualiztions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #load tables into pandas df
-diffs = pd.read_csv("SortedDiffs.csv")
+diffs = pd.read_csv("DifferenceTable.csv")
 kb = pd.read_csv("KidzBopTable.csv")
 original = pd.read_csv('OriginalTable.csv')
 
@@ -13,11 +13,11 @@ kbPop = kb['Popularity']
 orPop = original['Popularity']
 
 plt.subplot(1,2,1)
-plt.hist(kbPop, density=True, color='red')
+plt.hist(kbPop, density=True, color='red', label='Kidz Bop')
 plt.title("Kidz Bop Popularity")
 
 plt.subplot(1,2,2)
-plt.hist(orPop, density=True)
+plt.hist(orPop, density=True, label='Original')
 plt.title("Original Popularity")
 plt.subplots_adjust(wspace=.5)
 plt.show()
@@ -28,10 +28,21 @@ kbSent = kb['SentimentAverage'] * 100
 orSent = original['SentimentAverage'] * 100
 orSentSwear = original['SwearlessSentimentAverage'] * 100
 diffSent = diffs['DiffSentimentAverage'] * 100
+diffSentSwearless = diffs['DiffSwearlessSentimentAverage'] * 100
 
-plt.subplot(1,3,1)
-plt.hist(kbSent, alpha=0.5, density=True, color='red')
-plt.hist(orSent, alpha=0.5, density=True)
+plt.subplot(1,2,1)
+plt.hist(kbSent, alpha=0.5, density=True, color='red', label='KidzBop')
+plt.hist(orSent, alpha=0.5, density=True, label='Original')
 plt.legend(loc='upper left')
 plt.title("Average Sentiment Distributions")
+
+plt.subplot(1,2,2)
+plt.hist(diffSent, alpha=0.5, density=True, color='coral', label='Includes Swears')
+plt.hist(diffSentSwearless, alpha=0.5, density=True, color='green', label='Swearless')
+plt.title('Distribution of Sentiment Differences')
+plt.legend(loc='upper left')
+plt.subplots_adjust(wspace=.5)
 plt.show()
+
+
+
