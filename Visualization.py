4f0301cab160fc8d47a396773a81ef4e716cc89d
Garrett Kaufmann <31054430+gtdlk20@users.noreply.github.com>
2019-12-04 22:01:28 +0100

    fixed label orientation

--- Visualization.py
+++ Visualization.py
@@ -98,8 +98,9 @@ for i, year in enumerate(years):
     yearGenres = np.concatenate([genre for index, genre in enumerate(allGenres) if list(dfKidzBop["ReleaseDate"])[index][:-6] == year])
     topGenres = [genre for genre, _ in Counter(yearGenres).most_common(10)]
     genres = [yearGenre for yearGenre in yearGenres if yearGenre in topGenres]
-    print(genres)
-    plt.hist(genres, density = True, label = year)
+
+    plt.hist(genres, density=True, label=year)
+    plt.xticks(rotation=90)
     plt.title("Popular genres in %s" % year)
     plt.show()
 
