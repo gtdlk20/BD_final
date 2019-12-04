9ae7aeff8a5310bb6ab15ed3e02f59615d92d5b5
kevinhpinkerton@gmail.com <kevinhpinkerton@gmail.com>
2019-12-04 21:30:40 +0100

    Commit

--- visualiztions.py
+++ visualiztions.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import wordcloud
+import wordcloud 
 from ast import literal_eval
 import numpy as np
+import re
 
 #load tables into pandas df
-diffs = pd.read_csv("DifferenceTable.csv")
-kb = pd.read_csv("KidzBopTable.csv")
-original = pd.read_csv('OriginalTable.csv')
+dfDiff = pd.read_csv("DifferenceTable.csv")
+dfKidzBop = pd.read_csv("KidzBopTable.csv")
+dfOriginal = pd.read_csv('OriginalTable.csv')
 
-#create first plot
+#create first plot 
 plt.figure(1)
-#first plot: popularity of kidzbop versus original
-kbPop = kb['Popularity']
-orPop = original['Popularity']
+#first plot: popularity of kidzbop versus dfOriginal
+kbPop = dfKidzBop['Popularity']
+orPop = dfOriginal['Popularity']
 
 plt.subplot(1,2,1)
 plt.hist(kbPop, density=True, color='red', label='Kidz Bop')
@@ -27,11 +28,11 @@ plt.show()
 
 #second plot: Difference in sentiment 
 plt.figure(2)
-kbSent = kb['SentimentAverage'] * 100
-orSent = original['SentimentAverage'] * 100
-orSentSwear = original['SwearlessSentimentAverage'] * 100
-diffSent = diffs['DiffSentimentAverage'] * 100
-diffSentSwearless = diffs['DiffSwearlessSentimentAverage'] * 100
+kbSent = dfKidzBop['SentimentAverage'] * 100
+orSent = dfOriginal['SentimentAverage'] * 100
+orSentSwear = dfOriginal['SwearlessSentimentAverage'] * 100
+diffSent = dfDiff['DiffSentimentAverage'] * 100
+diffSentSwearless = dfDiff['DiffSwearlessSentimentAverage'] * 100
 
 plt.subplot(1,2,1)
 plt.hist(kbSent, alpha=0.5, density=True, color='red', label='KidzBop')
@@ -51,14 +52,13 @@ plt.show()
 plt.figure(3)
 
 #take all words which have been removed from the explicit texts and add them to removed
-wordsRemoved = list(map(lambda stringy: literal_eval(stringy), list(diffs["WordsRemoved"])))
-removedList = [wordRemoved for index, wordRemoved in enumerate(wordsRemoved) if list(diffs["DiffWordsAverage"])[index] < 30]
+wordsRemoved = list(map(lambda stringy: literal_eval(stringy), list(dfDiff["WordsRemoved"])))
+removedList = [wordRemoved for index, wordRemoved in enumerate(wordsRemoved) if list(dfDiff["DiffWordsAverage"])[index] < 30]
 removed = " ".join(np.concatenate(removedList)) 
 
-
 #take all words which replaced explicit text and add to added
-wordsAdded = list(map(lambda stringy: literal_eval(stringy), list(diffs["WordsAdded"])))
-addedList = [wordAdded for index, wordAdded in enumerate(wordsAdded) if list(diffs["DiffWordsAverage"])[index] < 30]
+wordsAdded = list(map(lambda stringy: literal_eval(stringy), list(dfDiff["WordsAdded"])))
+addedList = [wordAdded for index, wordAdded in enumerate(wordsAdded) if list(dfDiff["DiffWordsAverage"])[index] < 30]
 added = " ".join(np.concatenate(addedList)) 
 
 #create wordcloud objects
@@ -74,25 +74,27 @@ plt.subplot(1,2,2)
 plt.imshow(addedWC, interpolation='bilinear')
 plt.title("Words Used to Replace Explicit Lyrics")
 plt.show()
-
  
 plt.figure(4, figsize=(12, 7))
 #plot 4: show differences in song qualities
 for i, word in enumerate(['Acousticness','Danceability','Energy','Instrumentalness','Liveness','Loudness','Speechiness','Valence','Tempo']):
     plt.subplot(9,1,i+1)
-    plt.plot(kb[word], 'r', alpha=.5, label='KidzBop')
-    plt.plot(original[word], 'b', alpha=.5, label='Original')
+    plt.plot(dfKidzBop[word], 'r', alpha=.5, label='KidzBop')
+    plt.plot(dfOriginal[word], 'b', alpha=.5, label='Original')
     if i == 0:
         plt.legend(loc='upper right') 
     plt.title(word)
 plt.subplots_adjust(hspace=1.2)
 plt.show()
 
-
 #plot 5: distribution of genre popularity over time
 plt.figure(5)
 
-genres = list(map(lambda stringy: literal_eval(stringy), list(original["Genres"])))
-print(genres)
+genres = list(map(lambda stringy: literal_eval(stringy), list(dfOriginal["Genres"])))
+years = np.unique(list(map(lambda date: date[:-6], list(dfKidzBop["ReleaseDate"]))))
+for year in years:
+    yearGenre = np.concatenate([genre for index, genre in enumerate(genres) if list(dfKidzBop["ReleaseDate"])[index][:-6] == year])
+    plt.hist(yearGenre, density = True, label = year)
+
 
 
